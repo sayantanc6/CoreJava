6f8c6a825194db7a2f6621fc3ce937b57887b813
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2020-09-21 09:55:43 +0530

    na

--- DemoTryout/src/concurrency/EntryPoint.java
+++ DemoTryout/src/concurrency/EntryPoint.java
@@ -1,6 +1,11 @@
 package concurrency;
 
 public class EntryPoint {
+	/* Source of information :- 
+	 * https://stackoverflow.com/questions/13249835/java-does-wait-release-lock-from-synchronized-block#:~:text=When%20thread%20calls%20wait%20it,than%20goes%20to%20WAITING%20state.&text=So%20to%20summarize%2C%20thread%20acquires,synchronized%20method%20after%20the%20wait.
+	 * https://docs.oracle.com/javase/7/docs/api/java/lang/Object.html#wait()
+	 * https://howtodoinjava.com/java/multi-threading/multithreading-difference-between-lock-and-monitor/
+	 */
 
 	public static void main(String[] args) {
 		SharedResourceLockScheduling srls =new SharedResourceLockScheduling();
