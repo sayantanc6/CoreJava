69932fa6f354c17c7c3fc1f9fe67486c23a22873
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2020-09-24 10:33:46 +0530

    na
    na

--- DemoTryout/src/jpacustom/MyCustomRepository.java
+++ DemoTryout/src/jpacustom/MyCustomRepository.java
@@ -2,12 +2,17 @@ package jpacustom;
 
 import java.io.Serializable;
 import java.util.List;
+// import java.util.concurrent.Future;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+// import org.springframework.scheduling.annotation.Async;
 
 @NoRepositoryBean
 public interface MyCustomRepository<T, ID extends Serializable> extends JpaRepository<T, ID> {
 
-    List<T> findByAttributeContainsText(String attributeName, String text);
+//	@Async
+//    Future<List<T>> findByAttributeContainsText(String attributeName, String text); 
+	
+    List<T> findByAttributeContainsText(String attributeName, String text); 
 }
