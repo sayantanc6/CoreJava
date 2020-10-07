7ab9f32a969c97ff139ca099f357000ff0088d00
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-10-07 12:25:34 +0530

    updated_4

---
+++ DemoTryout/src/jwt/JwtBuildAndVerify.java
@@ -0,0 +1,40 @@
+package jwt;
+
+import java.util.Date;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+
+public class JwtBuildAndVerify {
+	
+	String jwt = "";
+
+	public JwtBuildAndVerify() {
+		super();
+	}
+
+	public String jwtBuild() {
+		
+		jwt =Jwts.builder()
+					  .setSubject("my subject")
+					  .setExpiration(new Date(1300819380))
+					  .claim("name", "Robert Token Man")
+					  .claim("scope", "self groups/admins")
+					  .signWith(SignatureAlgorithm.HS256, "secret".getBytes()) 
+					  .compact();
+		System.out.println("builded jwt : "+jwt); 
+		return jwt;
+	}
+	
+	public boolean verify(String jwt) throws SignatureException {
+		Jws<Claims> claims = Jwts.parser()
+								.setSigningKey("secret".getBytes())
+								.parseClaimsJws(jwt);
+		String scope = claims.getBody().get("scope", String.class); 
+		
+		return scope == "self groups/admins";
+	}
+}
---
+++ DemoTryout/src/jwt/Jwtentrypoint.java
@@ -0,0 +1,30 @@
+package jwt;
+
+import io.jsonwebtoken.SignatureException;
+
+public class Jwtentrypoint {
+	
+	/*
+	 * source of information :-
+	 * https://stormpath.com/blog/beginners-guide-jwts-in-java
+	 * https://www.baeldung.com/java-json-web-tokens-jjwt
+	 * */
+
+	public static void main(String[] args) {
+		JwtBuildAndVerify bav = new JwtBuildAndVerify();
+		String jwt = bav.jwtBuild();
+		boolean isverified = true;
+		try {
+			bav.verify(jwt);
+		} catch (SignatureException e) {
+			isverified = false;
+			System.out.println("not verified : "+e);			
+			e.printStackTrace();
+		}
+		
+		if (isverified) {
+			System.out.println("verified");
+		}
+	}
+ 
+}
