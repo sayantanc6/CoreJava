192b5f83b708820a0bff6965fb9f1c96279d3b50
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-10-22 09:48:29 +0530

    updated_most

--- DemoTryout/src/intertwinedbasicandoauth2ssso/repo/UserRepository.java
+++ DemoTryout/src/intertwinedbasicandoauth2ssso/repo/UserRepository.java
@@ -1,14 +1,21 @@
-package intertwinedbasicandoauth2ssso.repo;
+package com.example.demo;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
- 
+
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEmployee, Long> {
 	UserEmployee findByUsername(final String username);
@@ -23,5 +30,13 @@ public interface UserRepository extends JpaRepository<UserEmployee, Long> {
     public void updateLastLogin(@Param("lastLogin") Date lastLogin);
     
     public boolean existsByEmail(String email);
+    
+    @Async(AppConfiguration.TASK_EXECUTOR_REPOSITORY)
+    @Query("select e.firstName as "+ CustomEmployeeRs.USERNAME +"," +
+            " e.lastName as " + CustomEmployeeRs.PASSWORD + ", " +
+            " d.name as " + CustomEmployeeRs.ORGANIZATION_NAME +
+            " from Employee e join e.department d") 
+    public CompletableFuture<Page<List<Map<String, Object>>>> findUserDetails(final Pageable pageable); 
 
 }
+
