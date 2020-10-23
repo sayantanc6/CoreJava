cb009ec4530f11bf806821ef36784b40f3d53607
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-10-23 20:02:36 +0530

    most_updated

--- DemoTryout/src/codility/AngryFrogs.java
+++ DemoTryout/src/codility/AngryFrogs.java
@@ -1,22 +1,49 @@
 package codility;
 
+import java.util.Scanner;
 
 public class AngryFrogs {
 	
-	int index_max =0;
-
-	public int solution(int[] blocks) {
- 
-        int max = blocks[this.index_max];  
-		for (int i = this.index_max; i < blocks.length; i++) { 
-			if (blocks[i] > max) {
-				max = blocks[i];
-				this.index_max = i;
+	int initial_position_index=0;
+	int result =0;
+	int max;
+	
+	public int solution(int[] blocks) {		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("what is your initial position : ");
+		initial_position_index = scanner.nextInt();
+		
+		max = blocks[initial_position_index];
+		if(initial_position_index == 0 && initial_position_index < (blocks.length-1)) {
+			for (int i = 0; i < blocks.length; i++) {
+				if (blocks[i] > max) {
+					 max = blocks[i];
+					 result = i+1;
+				}
+			}
+		}else if(initial_position_index > 0 && initial_position_index < (blocks.length -1)) { 
+			for (int i = initial_position_index; i < blocks.length; i++) {
+				if (blocks[i] > max) {
+					 max = blocks[i];
+					 result = i;
+				}
+			}
+//			max = blocks[initial_position_index];
+//			for (int j = initial_position_index; j >=0; j--) { 
+//				if (blocks[j] > max) {
+//					 max = blocks[j];
+//					 result += j;
+//				}
+//			}
+		}else {
+			for (int i = (blocks.length-1); i >= 0; i--) {
+				if (blocks[i] > max) {
+					max = blocks[i];
+					 result = i;
+				}
 			}
 		}
-		if (blocks.length-1 > this.index_max) { 
-			 this.index_max+=1;
-		}
-		return this.index_max;  
+		return result;
 	}
 }
