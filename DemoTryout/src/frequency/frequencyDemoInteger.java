8f2815523abddf7ffcb81a9846a0452971800622
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-10-23 10:01:58 +0530

    updated_most

---
+++ DemoTryout/src/frequency/frequencyDemoInteger.java
@@ -0,0 +1,35 @@
+package frequency;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class frequencyDemoInteger {
+
+	public static void main(String[] args) {
+		
+		int[] s1 = {9,3,9,3,9,7};
+		int result = 0;
+		Map<Integer, Integer> myMap = new HashMap<Integer, Integer>();
+		for (int element : s1) {  
+			if (myMap.containsKey(element)) {
+				myMap.replace(element, myMap.get(element), myMap.get(element) + 1); 
+			} else {
+				myMap.put(element, 1);  
+			}
+		}
+		System.out.println(myMap); 
+		for (Map.Entry<Integer, Integer> entry : myMap.entrySet()) {
+			 if (entry.getValue() == 1) {
+				 result = entry.getKey();
+			}
+		}
+		System.out.println(result); 
+//		System.out.println("duplicate occurences are :-");
+//		myMap.entrySet().forEach(entry -> {
+//			if (entry.getValue() == 2) {
+//				System.out.print(entry.getKey().toString()+" "); 
+//			}
+//		});
+	}
+
+}
