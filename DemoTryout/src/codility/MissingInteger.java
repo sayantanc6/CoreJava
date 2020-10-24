b07cd2e1674892fb976504f23a374f5829299e24
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-10-24 06:35:44 +0530

    most_updated

--- DemoTryout/src/codility/MissingInteger.java
+++ DemoTryout/src/codility/MissingInteger.java
@@ -3,13 +3,13 @@ package codility;
 import java.util.Arrays;
 
 
-public class SmallestPositiveInteger {
+public class MissingInteger {
 
 	public static void main(String[] args) {
-		int[] a = {-3,-2,0,1,1,5,3,4};
-		int min =0;
-		a = Arrays.stream(a).filter(x -> x >=0).sorted().distinct().toArray();
-		
+		int[] a = {1, 3, 6, 4, 1, 2}; 
+		int min =1;
+		a = Arrays.stream(a).filter(x -> x > 0).sorted().distinct().toArray();
+
 		for (int i : a) {
 			if (min == i) {
 				min++;
