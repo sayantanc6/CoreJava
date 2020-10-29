2a4a5bda2d5149b4200a180d0453d9afdf982dba
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-10-29 18:11:23 +0530

    most_updated

---
+++ DemoTryout/src/codility/TapeEquilibrium.java
@@ -0,0 +1,32 @@
+package codility;
+
+public class TapeEquilibrium {
+	
+	/*
+	 * source of information :-
+	 * https://github.com/Mickey0521/Codility/blob/master/TapeEquilibrium.java
+	 * */
+
+	public static void main(String[] args) {
+		int[] A= {3,1,2,4,3};
+		int totalSum =0;
+		int operand1 =0;
+		int operand2 =0;
+		int mindiff =Integer.MAX_VALUE;
+		int diff=0;
+		
+		for (int i = 0; i < A.length; i++)
+			totalSum += A[i];
+		
+		for (int p = 1; p < A.length; p++) {
+			 operand1 += A[p - 1];
+			 operand2 = totalSum - operand1;
+			 diff = operand1 - operand2;
+			 if (diff < 0) 
+				diff = -diff; 
+			 
+			 mindiff = Math.min(diff, mindiff);
+		}
+		System.out.println(mindiff); 
+	}
+}
