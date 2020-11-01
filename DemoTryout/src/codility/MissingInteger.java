dbe04196ea458bab182ab2860e99ed600dd492c0
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-11-01 18:38:45 +0530

    updated_most

--- DemoTryout/src/codility/MissingInteger.java
+++ DemoTryout/src/codility/MissingInteger.java
@@ -1,7 +1,7 @@
 package codility;
 import java.util.Arrays;
  
-public class SmallestPositiveMissingInteger {
+public class MissingInteger {
 
 	public static void main(String[] args) {
 		int[] a = {1, 3, 6, 4, 1, 2};   
