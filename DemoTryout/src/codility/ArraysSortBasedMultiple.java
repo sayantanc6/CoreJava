848001cbfd3e19fcbbefe42d808eafaee43183db
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-11-04 18:24:11 +0530

    most_updated

--- DemoTryout/src/codility/ArraysSortBasedMultiple.java
+++ DemoTryout/src/codility/ArraysSortBasedMultiple.java
@@ -11,6 +11,7 @@ public class ArraysSortBasedMultiple {
 		char[] letters = {'b','a','d','c'};
 		String[] words = {"ball","apple","dog","cat"}; 
 		int[] alphabeticalorder = {2,1,4,3};
+		char[] sortedletters = new char[letters.length];
 		int[] sortedalphabeticalorder = new int[letters.length];
 		String[] sortedwords = new String[letters.length];
 		List<Character> lettersList = new ArrayList<>();
@@ -27,14 +28,15 @@ public class ArraysSortBasedMultiple {
 		for (Character character : sortedlettersList) { 
 			sorted_extracted_index = lettersList.indexOf(character);
 			current_index = sortedlettersList.indexOf(character);
+			sortedletters[sorted_extracted_index] = letters[current_index];
 			sortedwords[sorted_extracted_index] = words[current_index]; 
 			sortedalphabeticalorder[sorted_extracted_index] = alphabeticalorder[current_index];    
 		}
 		
 		System.out.println("letters : "+Arrays.toString(letters)); 
-		System.out.println("words : "+Arrays.toString(words));
+		System.out.println("words : "+Arrays.toString(words)); 
 		System.out.println("alphabetical order : "+Arrays.toString(alphabeticalorder));
-		System.out.println("sorted letters list : "+Arrays.toString(sortedlettersList.toArray()));
+		System.out.println("sorted letters : "+Arrays.toString(sortedletters)); 
 		System.out.println("sorted words : "+Arrays.toString(sortedwords));
 		System.out.println("sorted alphabetical order : "+Arrays.toString(sortedalphabeticalorder));
 	}
