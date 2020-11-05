fcb0a3a023ee0331f8cd079caa046c2bf0386d08
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-11-05 13:18:52 +0530

    most_updated

--- DemoTryout/src/codility/Dominator.java
+++ DemoTryout/src/codility/Dominator.java
@@ -1,19 +1,14 @@
 package codility;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 public class Dominator { 
 
 	public static void main(String[] args) { 
 		int[] A = {3,4,3,2,3,-1,3,3};
 		Map<Integer, Integer> myMap = new ConcurrentHashMap<Integer,Integer>(); 
-		Map<Integer,Integer> sortedMap = new HashMap<>();
 		int max =0;
 		int dominator =0;
 		
@@ -33,9 +28,6 @@ public class Dominator {
 				dominator =-1;
 		}
 		System.out.println(myMap); 
-		sortedMap = myMap.entrySet().stream().sorted(Entry.comparingByValue((a,b)-> b-a)) 
-			    	.collect(Collectors.toMap(Entry::getKey, Entry::getValue,(e1, e2) -> e1, LinkedHashMap::new));
-		System.out.println(sortedMap); 
 		
 		if (dominator ==0) {
 			for (int i = 0; i < A.length; i++) {
