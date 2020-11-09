d703ab023037724dedddbfb8a61342e39915008b
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-11-09 09:37:49 +0530

    most_updated

--- DemoTryout/src/codility/CountFactors.java
+++ DemoTryout/src/codility/CountFactors.java
@@ -4,12 +4,28 @@ public class CountFactors {
 
 	public static void main(String[] args) {
 		
-		int N = 24;
-		int count =0;
+		int N = 27043111;
+		int count =2;
 		
-		for (int i = 1; i <= N; i++) {
-			if (N%i ==0)  
-				count++;
+		if (N ==1) 
+			count =1;
+		
+		if (N ==2) 
+			count =2;
+		
+		if (N%2 ==0) 
+			count++;
+		
+		if (N < 50) {
+			for (int i = 3; i <= Math.ceil(N/2); i++) {
+				if (N % i == 0)
+					count++;
+			} 
+		}else {
+			for (int i = 3; i <= Math.ceil(N/2); i++) {
+				if (N % i == 0)
+					count++; 
+			}
 		}
 		System.out.println(count);
 	}
