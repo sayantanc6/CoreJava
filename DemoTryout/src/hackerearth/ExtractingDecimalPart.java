9df5bbd318e94e214fcad408ff0b9f7f778ab222
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-11-10 10:52:04 +0530

    most_updated

---
+++ DemoTryout/src/hackerearth/ExtractingDecimalPart.java
@@ -0,0 +1,12 @@
+package hackerearth;
+
+public class ExtractingDecimalPart {
+
+	public static void main(String[] args) {
+		float n = 0;
+		System.out.println(n % 1);
+		System.out.println(n - Math.floor(n));
+		System.out.println(n - (int)n);
+	}
+
+}
